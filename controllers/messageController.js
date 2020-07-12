c5b8301ce8d38ebc88eed379fab80e0736c549be
Christophe Duchesne <christophe.duchesne.96@gmail.com>
2020-07-12 18:00:41 -0400

    implemented POST on /message and linked test

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,18 @@
+const { Wit, log } = require('node-wit');
 // TODO : require Chatbot class instance
 
 exports.extractFromMessage = function (req, res) {
-    res.send('NOT IMPLEMENTED: extractFromMessage');
+
+    // TODO : encapsulate all the following logic by a call on getMessageIntention() on a chatbot class/instance
+
+    let message = req.body.message;
+
+    const client = new Wit({
+        accessToken: "GKH7ZZ26SXF2SPO6OEV2W6PHHBZRXFHW",
+        //logger: new log.Logger(log.DEBUG) // optional
+    });
+
+    client.message(message).then(response =>{
+        res.json(response);
+    })
 };
---
+++ tests/routes.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('../app')
+describe('Post Endpoints', () => {
+  it('should test that sending a post to /message works', async () => {
+    const res = await request(app)
+      .post('/message')
+      .send({
+        message: "this is a test",
+      })
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('text', "this is a test");
+  })
+})
