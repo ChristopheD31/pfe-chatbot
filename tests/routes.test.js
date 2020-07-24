368135ac48bcb69b517621d2a49140a77e43de3a
Christophe Duchesne <christophe.duchesne.96@gmail.com>
2020-07-23 23:33:34 -0400

    trying fix for test not passing

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest')
 const app = require('../app')
 describe('Post Endpoints', () => {
-  it('should test that sending a post to /message works', async () => {
+  it('should test that sending a post to /intent works and reaches Wit.ai', async () => {
     const res = await request(app)
-      .post('/message')
+      .post('/message/intent')
       .send({
         message: "this is a test",
       })
